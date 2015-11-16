0a11f22fbe618e0240078ed03eaa634b94af63a3
Edric Kyauk <edric.kyauk@gmail.com>
2015-11-16 01:11:55 -0800

    I give up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
-    before_filter :check_login
-
-    def check_login
-        if session[:linkedin_token] == nil || session[:linkedin_pin] == nil
-            redirect_to :login
-        end
-    end
 
 end
--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -1,68 +1,89 @@
 class ScrapeController < ApplicationController
-    skip_before_filter :check_login, :except => [:index]
-
+    # before_filter :check_login, :only => :index
 
     def index
     end
 
     def login
-        linkedinClient = LinkedIn::Client.new(ENV['LINKEDIN_CLIENT_ID'], ENV['LINKEDIN_SECRET'])
-
-        request_token = linkedinClient.request_token({}, :scope => "r_basicprofile r_emailaddress")
+        if session[:request_token] != nil
+            redirect_to :action => :index
+            return
+        end
+        client = LinkedIn::Client.new(ENV['LINKEDIN_CLIENT_ID'], ENV['LINKEDIN_SECRET'])
+        request_token = client.request_token({:oauth_callback => ENV['CALLBACK_URL']}, :scope => "r_basicprofile r_emailaddress")
         session[:request_token] = request_token.token
         session[:request_secret] = request_token.secret
         redirect_to request_token.authorize_url
     end
 
     def callback
-        linkedinClient = LinkedIn::Client.new(ENV['LINKEDIN_CLIENT_ID'], ENV['LINKEDIN_SECRET'])
+        client = LinkedIn::Client.new(ENV['LINKEDIN_CLIENT_ID'], ENV['LINKEDIN_SECRET'])
+        puts 'CALLBACK'
         if session[:oauth_token].nil?
             pin = params[:oauth_verifier]
             oauth_token, oauth_secret = client.authorize_from_request(session[:request_token], session[:request_secret], pin)
             session[:oauth_token] = oauth_token
             session[:oauth_secret] = oauth_secret
         end
-        redirect_to :index
+        redirect_to :action => :index
     end
 
-    # def view
-    #     url = params[:url]
-    #     @profile_table = []
-    #     profile = Linkedin::Profile.get_profile(url)
-    #     puts profile
-    #     if profile == nil
-    #         render :inline => "<tr>Cannot find #{url}. Try again later.</tr>"
-    #     end
-    #     name = profile.name.empty? == nil ? profile.first_name + ' ' + profile.last_name : profile.name
-    #     if !profile.education.empty?
-    #         for education_info in profile.education
-    #             @profile_table << education_line(name, education_info)
-    #         end
-    #     end
-
-    #     if !profile.current_companies.empty?
-    #         puts profile.current_companies
-    #         for company_info in profile.current_companies
-    #             @profile_table << company_line(name, company_info)
-    #         end
-    #     end
-    #     if !profile.past_companies.empty?
-    #         for company_info in profile.past_companies
-    #             @profile_table << company_line(name, company_info)
-    #         end
-    #     end
-    #     puts 'hi~'
-    #     puts @profile_table
-    #     render partial: 'profile_table'
-    # end
+    def view
+        url = params[:url]
+        @profile_table = []
+        user = linkedin_client.profile(:url => url)
+        puts user
+        puts 'hiii'
+        # if profile == nil
+        #     render :inline => "<tr>Cannot find #{url}. Try again later.</tr>"
+        # end
+        # name = profile.name.empty? == nil ? profile.first_name + ' ' + profile.last_name : profile.name
+        # if !profile.education.empty?
+        #     for education_info in profile.education
+        #         @profile_table << education_line(name, education_info)
+        #     end
+        # end
+        positions = linkedin_client.profile(:url => url, :fields => %w(positions))
+        education = linkedin_client.profile(:url => url, :fields => %w(education))
+        puts "EDUCAMATA #{education}"
+        if positions != nil
+            puts positions
+            for positon in positions
+                start_date = "#{position[:start_date][:month]/positon[:start_date][:year]}"
+                end_date = positon[:is_current] ? "" : "#{position[:end_date][:month]/positon[:end_date][:year]}"
+                @profile_table << company_line(position[:title], positon[:company], start_date, end_date)
+            end
+        end
+        if !profile.past_companies.empty?
+            company_line(title, company_info, start_date, end_date)
+            for company_info in profile.past_companies
+                @profile_table << company_line(name, company_info)
+            end
+        end
+        puts 'hi~'
+        puts @profile_table
+        render partial: 'profile_table'
+    end
 
 private
+    def linkedin_client
+        client = LinkedIn::Client.new(ENV['LINKEDIN_CLIENT_ID'], ENV['LINKEDIN_SECRET'])
+        client.authorize_from_access(session[:oauth_token], session[:oauth_secret])
+        return client
+    end
+
+    def check_login
+        if session[:oauth_token] == nil || session[:oauth_verifier] == nil
+            redirect_to :login
+        end
+    end
+
     def education_line(name, education_info)
         return [name, 'Degree', education_info[:degree], education_info[:major], education_info[:name]]
     end
 
-    def company_line(name, company_info)
-        return [name, 'Employment', nil, nil, nil, company_info[:company], company_info[:title], company_info[:industry], company_info[:linkedin_company_url], company_info[:company_size], company_info[:start_date].to_s, company_info[:end_date].to_s]
+    def company_line(title, company_info, start_date, end_date)
+        return [company_info[:name], 'Employment', nil, nil, nil, company_info[:company], title, company_info[:type], company_info[:linkedin_company_url], company_info[:size], start_date, end_date]
     end
 
 end
